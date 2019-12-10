adab011a26b9af24e5287131b51c3988ed4bd2af
VincentLembo <vincent.lembo@optisantis.com>
2019-12-10 09:14:01 +0100

    move images folder inside src

--- memory2.0/src/Main.tsx
+++ memory2.0/src/Main.tsx
@@ -5,6 +5,22 @@ import './styles/tailwind.css';
 import './styles/memory.scss';
 
 const Main = () => {
+    const fruitsLegumes = require.context('./assets/fruits_legumes', true, /\.png$/);
+    const medical = require.context('./assets/medical', true, /\.png$/);
+    const meteo = require.context('./assets/meteo', true, /\.png$/);
+    const sommeil = require.context('./assets/sommeil', true, /\.png$/);
+    const sport = require.context('./assets/sport', true, /\.png$/);
+
+    const paths = [fruitsLegumes.keys(), medical.keys(), meteo.keys(), sommeil.keys(), sport.keys()];
+
+    const fruitsLegumesImages = paths[0].map((path) => fruitsLegumes(path));
+    const medicalImages = paths[1].map((path) => medical(path));
+    const meteoImages = paths[2].map((path) => meteo(path));
+    const sommeilImages = paths[3].map((path) => sommeil(path));
+    const sportImages = paths[4].map((path) => sport(path));
+
+    console.log(fruitsLegumesImages);
+
     let numbers = [12, 16, 20, 24, 26];
 
     // On type useState quand il y a deux types possible.
@@ -45,6 +61,13 @@ const Main = () => {
         <div>
             <div>{renderBtns()}</div>
             <div>{renderCards()}</div>
+            <div>
+                <img src={fruitsLegumesImages[2]} alt="pl" />;
+                <img src={medicalImages[2]} alt="pl" />;
+                <img src={meteoImages[2]} alt="pl" />;
+                <img src={sommeilImages[2]} alt="pl" />;
+                <img src={sportImages[2]} alt="pl" />;
+            </div>
         </div>
     );
 };
