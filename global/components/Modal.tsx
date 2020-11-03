6c3c4fe5737fb946d01901a92919ee4a08a26030
vincelemb <vincent.lembo@outlook.fr>
2020-11-03 11:36:06 +0100

    heroku setup

--- global/components/Modal.tsx
+++ global/components/Modal.tsx
@@ -18,7 +18,7 @@ const Modal: React.FC<ModalProps> = (props: React.PropsWithChildren<ModalProps>)
     return (
         <React.Fragment>
             {props.isModal === true && (
-                <div className="_fixed _top-0 _bottom-0 _left-0 _right-0 _z-50 _w-full _h-full _justify-center _items-center _flex" 
+                <div className="_fixed  _top-0 _bottom-0 _left-0 _right-0 _z-50 _w-full _h-full _justify-center _items-center _flex" 
                 style={{backgroundColor:`${props.isOverlay === true ? "rgba(0,0,0,.8)" : "transparent"}`}}>
                     <button className="_absolute _top-0 _right-0 _m-sm" onClick={props.onCloseBtnClick && props.onCloseBtnClick}><CloseSvg svgWidth="25px"></CloseSvg></button>
                     <div className="_flex-col _flex _justify-center _items-center _absolute _h-auto _p-sm">
